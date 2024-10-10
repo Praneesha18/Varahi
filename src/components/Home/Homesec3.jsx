09a714aa2671e0e64e9a52a730a0e73967b6b0af
Praneesha <pranesha182004@gmail.com>
2024-10-10 23:48:40 +0530

    updated urls

--- src/components/Home/Homesec3.jsx
+++ src/components/Home/Homesec3.jsx
@@ -15,43 +15,14 @@ import {
 } from "@chakra-ui/react";
 import { ContextProvider } from "../../data/ProductDetails";
 import theme from "../../theme";
-import img1 from "../../assets/pickles/chickenbone.webp";
-import img2 from "../../assets/pickles/chickenboneless.webp";
-import img3 from "../../assets/pickles/chicken-keema.jpg";
-import img4 from "../../assets/pickles/muttonbone.webp";
-import img5 from "../../assets/pickles/muttonboneless.webp";
-import img6 from "../../assets/pickles/prawns.webp";
-import img7 from "../../assets/specialpickles/gongurachicken.webp";
-import img8 from "../../assets/specialpickles/gonguramutton.webp";
-import img9 from "../../assets/specialpickles/gonguraprawns.webp";
-import img10 from "../../assets/podi/kandipodi.webp";
-import img11 from "../../assets/podi/moringapodi.avif";
-import img12 from "../../assets/podi/curryleavespodi.jpeg";
-import img13 from "../../assets/podi/flaxseedspodi.jpg";
-import img14 from "../../assets/podi/vellulikarampodi.jpg";
 import { FaShoppingCart } from "react-icons/fa";
 import Singlepage from "../../pages/Singlepage";
 import { Link, NavLink } from "react-router-dom";
 const Homesec3 = () => {
   const data = useContext(ContextProvider);
   const { pickles, podis, cart } = data;
-  const images = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-    img8,
-    img9,
-    img10,
-    img11,
-    img12,
-    img13,
-    img14,
-  ];
-  const message = encodeURIComponent("Hello, I am interested in your products!");
+
+  
   return (
     <Box
       w="95%"
@@ -75,7 +46,7 @@ const Homesec3 = () => {
               <CardBody>
                 <Box w="100%" h={{base:'100px',md:'200px'}}>
                   <Image
-                    src={images[index]}
+                    src={pickle.image}
                     alt={pickle.name}
                     borderRadius="lg"
                     w='100%'
@@ -125,7 +96,7 @@ const Homesec3 = () => {
               <CardBody>
                 <Box w="100%" h={{base:'100px',md:'200px'}}>
                   <Image
-                    src={images[index+9]}
+                    src="/images/pickles/chickenbone.webp"
                     alt={podi.name}
                     borderRadius="lg"
                     w='100%'
--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,14 +1,87 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import { Box } from '@chakra-ui/react'
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Box, Image, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react";
+import { ContextProvider } from "../data/ProductDetails";
+import theme from "../theme";
+import { IoChevronDownCircleOutline } from "react-icons/io5";
 const Singlepage = () => {
-   const {id}=useParams()
+  const { pickles, podis } = useContext(ContextProvider);
+  const { subcategory, id } = useParams();
+  let category = subcategory === "pickles" ? pickles : podis;
+  console.log(category);
+  const mainproduct = category.filter(
+    (element) => element.id === parseInt(id, 10)
+  );
+
+  console.log(mainproduct);
   return (
-   <>
-  <Box >{id}</Box>
-   </>
-  )
-}
+    <>
+      <Box
+        w="95%"
+        maxW={"1200px"}
+        mx="auto"
+        fontFamily={theme.fonts.body}
+        boxShadow={"md"}
+      >
+        <Box
+          w="100%"
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          flexDirection={"column"}
+        >
+          <Box
+            w="100%"
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Box w="40%" h="300px">
+              <Image w="100%" h="100%"  />
+            </Box>
+            <Box
+              w="60%"
+              display="flex"
+              justifyContent={"flex-start"}
+              gap="10px"
+            >
+              <Text fontSize={{ base: "1rem", md: "2rem" }}>
+                {mainproduct.name}
+              </Text>
+              <Text fontSize={{ base: "1rem", md: "2rem" }}>
+                {mainproduct.price}
+              </Text>
+              <Box
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"flex-start"}
+                flexDirection={"column"}
+                gap="10px"
+              >
+                <Menu>
+                  <MenuButton  rightIcon={<IoChevronDownCircleOutline />}>
+                    weight
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>
+                     250gms
+                    </MenuItem>
+                    <MenuItem>
+                      500gms
+                    </MenuItem>
+                    <MenuItem>
+                      1kg
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
 export default Singlepage;
